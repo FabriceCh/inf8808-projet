0ff19767d56334052c72146e5502f9746a15153f
Philippe Carphin <philippe.carphin@polymtl.ca>
2019-03-13 21:29:31 -0400

    Made unit tests cover 100% of lines in pysc2.py

--- sc2/test_pysc2.py
+++ sc2/test_pysc2.py
@@ -3,6 +3,10 @@ import sc2reader
 import pysc2
 import matplotlib.pyplot as plt
 
+# THis helps the IDE
+import sc2reader.events
+import sc2reader.events.game
+
 class TestPysc2(unittest.TestCase):
 
     def __init__(self, methodName='runTest'):
@@ -11,11 +15,32 @@ class TestPysc2(unittest.TestCase):
         # once and we are never modifying the internal object.
         self.replay = pysc2.SC2ReplayWrapper('spawningtool_replays/dark_v_Solar_Game1_PortAleksanderLE.SC2Replay')
 
+    def test__select_from_list(self):
+        def selector(e):
+            return True
+        gen = pysc2.select_from_list(self.replay._replay.game_events, selector)
+        l = list(gen)
+        self.assertTrue(len(l) > 1000)
+
     def test_select_from_list(self):
         def selector(e):
             return True
         l = pysc2.select_from_list(self.replay._replay.game_events, selector)
         self.assertTrue(len(l) > 1000)
+
+
+    def test_select_from_game_events(self):
+        event_list = self.replay.select_from_game_events(lambda e: isinstance(e, sc2reader.events.game.GetControlGroupEvent))
+        self.assertIsInstance(event_list[0], sc2reader.events.game.GetControlGroupEvent)
+
+    def test_select_from_events(self):
+        def selector(e):
+            return isinstance(e, sc2reader.events.UnitBornEvent)
+
+        event_list = self.replay.select_from_events(selector)
+
+        self.assertIsInstance(event_list[0], sc2reader.events.UnitBornEvent)
+        self.assertTrue(len(event_list) > 80)
 
     def test_select_from_list_complex_selector(self):
 
@@ -24,11 +49,35 @@ class TestPysc2(unittest.TestCase):
         l = pysc2.select_from_list(self.replay._replay.game_events, selector)
         self.assertIsInstance(l[0], sc2reader.events.game.GetControlGroupEvent)
 
-    def test_bar_chart_get_list(self):
+    def test_categorize(self):
+
         def selector(e):
             return isinstance(e, sc2reader.events.game.GetControlGroupEvent)
-        event_list = pysc2.select_from_list(self.replay._replay.game_events, selector)
-        self.assertIsInstance(event_list[0], sc2reader.events.game.GetControlGroupEvent)
+        def category_map(e):
+            return e.control_group
+        event_list = self.replay.select_from_game_events(selector)
+
+        categories = self.replay.categorize(event_list, category_map=category_map)
+
+        self.assertTrue(categories[1] > 800)
+
+        def value_map(e):
+            return 2
+        categories = self.replay.categorize(event_list, category_map=category_map, value_map=value_map)
+        self.assertTrue(categories[1] > 1600)
+
+    def test_categorize_as_lists(self):
+        def selector(e):
+            return isinstance(e, sc2reader.events.game.GetControlGroupEvent)
+        def category_map(e):
+            return e.control_group
+        event_list = self.replay.select_from_game_events(selector)
+
+        category_counts = self.replay.categorize(event_list, category_map)
+        categorized_events = self.replay.categorize_as_lists(event_list, category_map)
+
+        self.assertTrue(len(categorized_events[2]) == category_counts[2])
+
 
     def test_bar_chart(self):
 
@@ -46,3 +95,10 @@ class TestPysc2(unittest.TestCase):
             ev_list=self.replay._replay.game_events,
             category_map=category_map
         )
+
+    def test_plot_locations(self):
+        event_list = self.replay.select_from_game_events(lambda e: isinstance(e, sc2reader.events.game.CameraEvent))
+
+        pysc2.plot_locations(event_list, "Title for the plot that I made so easily")
+
+        self.assertRaises(pysc2.Pysc2EmptyListError, pysc2.plot_locations, [], "Should not print")
