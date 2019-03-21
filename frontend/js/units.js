93b368caac0d91d4e012728832b12836e0825c46
Adam Martin-Côté <adammartincote@gmail.com>
2019-03-21 11:14:44 -0600

    creates the 2 top-level d3 functions

---
+++ frontend/js/units.js
@@ -0,0 +1,13 @@
+(function () {
+  "use strict";
+  let svg = d3.select("#units-viz").append("svg");
+
+  svg
+      .append("rect")
+      .attr("x", 50)
+      .attr("y", 50)
+      .attr("height", 100)
+      .attr("width", 100)
+      .attr("fill", "blue")
+  ;
+})();
