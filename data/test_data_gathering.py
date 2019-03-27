d8ce27e01a1b8014e5592babfb5992fdd73d5560
Philippe Carphin <philippe.carphin@polymtl.ca>
2019-03-27 12:51:47 -0400

    Created real data for unit composition dataviz

--- data/test_data_gathering.py
+++ data/test_data_gathering.py
@@ -13,19 +13,19 @@ class TestDataGathering(unittest.TestCase):
 
     def __init__(self, methodName='runTest'):
         super(TestDataGathering, self).__init__(methodName=methodName)
-        self.replay = pysc2.SC2ReplayWrapper('spawningtool_replays/dark_v_Solar_Game1_PortAleksanderLE.SC2Replay')
+        self.replay = pysc2.SC2ReplayWrapper('replays/pvpGame1.SC2Replay')
 
     def test_process_unit_lifetime_events(self):
         categories = self.replay.categorize_unit_lifetime_events()
         self.assertTrue(len(categories) == 4)
         for cat in categories:
-            self.assertTrue(len(categories[cat]) >= 50)
+            self.assertTrue(len(categories[cat]) >= 10)
 
     def test_get_unit_from_event(self):
         """ For more info, add a breakpoint on the last line of this test and
         inspect the unit_born_event and the found_events list """
         categories = self.replay.categorize_unit_lifetime_events()
-        a_unit_born_event = categories[sc2reader.events.tracker.UnitBornEvent][100]
+        a_unit_born_event = categories[sc2reader.events.tracker.UnitBornEvent][50]
         try:
             id_from_event = pysc2.get_unit_from_event(a_unit_born_event)
         except IndexError:
@@ -37,7 +37,7 @@ class TestDataGathering(unittest.TestCase):
     def test_unit_lifetime_post_processing(self):
         categories = self.replay.categorize_unit_lifetime_events()
         processed_data = pysc2.match_events_to_units(categories)
-        self.assertTrue(len(processed_data) > 900)
+        self.assertTrue(len(processed_data) > 10)
         pass
 
     def test_post_post_processing(self):
@@ -47,5 +47,11 @@ class TestDataGathering(unittest.TestCase):
         import json
         json.dumps(post_post_processed)
         print(post_post_processed)
+        # for unit in post_post_processed['p1']['unit_lifetimes']:
+        #     print(unit)
+        for unit in post_post_processed['p2']['unit_lifetimes']:
+            print(unit)
         print(len(post_post_processed))
+        with open('datafiles/unitcomposition/realdata.json', 'w+') as f:
+            f.write(json.dumps(post_post_processed, indent=2))
         pass
