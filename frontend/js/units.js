2ec112261747b14ff7a7eeccdd69fcc9233a12ac
Adam Martin-Côté <adammartincote@gmail.com>
2019-03-28 09:21:21 -0600

    cleared some of the warnings

--- frontend/js/units.js
+++ frontend/js/units.js
@@ -1,7 +1,7 @@
 (function () {
-  "use strict"
+  "use strict";
 
-  const filePath = "/data/unitcomposition/mockdata.json"
+  const filePath = "/data/unitcomposition/mockdata.json";
 
   d3.json(filePath).then(function (data) {
 
@@ -16,15 +16,15 @@
       left: 120,
       right: 0,
       bottom: 10
-    }
+    };
 
-    let width = 1344 - margin.left - margin.right
+    let width = 1344 - margin.left - margin.right;
 
     // Tooltip settings
     let tooltip = { 
       width: 250,
       spacing: 10
-    }
+    };
 
     let row = {
       // Margin between rows
@@ -32,23 +32,23 @@
         top: 10,
         bottom: 10
       }
-    }
+    };
     
     // Player column attributes
     let column = {
       gap: 20, // Gap between two columns
-    }
+    };
 
     // Lifetime line attributes
     let line = {
       height: 2,
       gap: 1
-    }
+    };
 
     // Game metadata
     let game = {
       duration: 30 * 60 // duration of the game in seconds
-    }
+    };
 
     /*
     |--------------------------------------------------------------------------
@@ -56,7 +56,7 @@
     |--------------------------------------------------------------------------
     */
 
-    data = generateRandomData(game.duration)
+    data = generateRandomData(game.duration);
 
     /*
     |--------------------------------------------------------------------------
@@ -69,13 +69,13 @@
     
     // For each unit, the height and vertial offset is calculated 
     // to correctly position the rows on the graph.
-    let offset = 0
+    let offset = 0;
     data.units = data.units.map(u => {
-      u.offset = offset + row.margin.top
-      u.height = d3.max(data.players, p =>p.unit_lifetimes[u.id].length) * (line.height+line.gap)
-      offset += u.height + row.margin.bottom
+      u.offset = offset + row.margin.top;
+      u.height = d3.max(data.players, p =>p.unit_lifetimes[u.id].length) * (line.height+line.gap);
+      offset += u.height + row.margin.bottom;
       return u
-    })
+    });
 
     // Sort unit_lifetimes 
     data.players.forEach(p => {
@@ -84,7 +84,7 @@
           return (a[0] > b[0]) ? 1 : -1
         })
       })
-    })
+    });
 
     /*
     |--------------------------------------------------------------------------
@@ -92,8 +92,8 @@
     |--------------------------------------------------------------------------
     */
 
-    let fullHeight = offset
-    let height = fullHeight - margin.top - margin.bottom
+    let fullHeight = offset;
+    let height = fullHeight - margin.top - margin.bottom;
 
     /*
     |--------------------------------------------------------------------------
@@ -104,12 +104,12 @@
     // Color scale (based on the unit category)
     let color = d3.scaleOrdinal()
     .domain(data.units.map(u => u.category))
-    .range(d3.schemeSet1)
+    .range(d3.schemeSet1);
 
     // x scales : for the two player columns
     let x = d3.scaleLinear()
     .domain([0, game.duration])
-    .range([0, width/2 - column.gap/2])
+    .range([0, width/2 - column.gap/2]);
 
     /*
     |--------------------------------------------------------------------------
@@ -117,11 +117,11 @@
     |--------------------------------------------------------------------------
     */
 
-    let svg = d3.select("#viz").attr("height", fullHeight)
+    let svg = d3.select("#viz").attr("height", fullHeight);
 
     let g = svg
       .append("g")
-      .attr("transform", `translate(${margin.left} ${margin.top})`)
+      .attr("transform", `translate(${margin.left} ${margin.top})`);
 
     /*
     |--------------------------------------------------------------------------
@@ -136,26 +136,26 @@
     .data(uniq(data.units.map(u => u.category)))
     .enter()
     .append("g")
-    .attr('transform', (d,i) => `translate(${i * 100},0)`)
+    .attr('transform', (d,i) => `translate(${i * 100},0)`);
 
     categories.append("circle")
     .attr("cx", 7)
     .attr("cy", 0)
     .attr("r", 7)
-    .attr("fill", d => color(d))
+    .attr("fill", d => color(d));
 
     categories.append("text")
     .attr("x", 20)
     .attr("y", 5)
-    .text(d => d.capitalize())
+    .text(d => d.capitalize());
 
-    let categoryOffset = 0
-    var nodeWidth = (d) => d.getBBox().width
+    let categoryOffset = 0;
+    let nodeWidth = (d) => d.getBBox().width;
     categories.attr('transform', function(d, i) {
-        let x = categoryOffset
-        categoryOffset += nodeWidth(this) + 15
+        let x = categoryOffset;
+        categoryOffset += nodeWidth(this) + 15;
         return `translate(${x},0)`
-    })
+    });
 
     /*
     |--------------------------------------------------------------------------
@@ -170,7 +170,7 @@
     .enter()
     .append("g")
     .attr("data-unit-id", d => d.id)
-    .attr("transform", d => `translate(0,${d.offset})`)
+    .attr("transform", d => `translate(0,${d.offset})`);
 
     /*
     |--------------------------------------------------------------------------
@@ -182,7 +182,7 @@
     .attr("x", -margin.left)
     .attr("y", 0)
     .text(d => d.name)
-    .attr("alignment-baseline", "hanging")
+    .attr("alignment-baseline", "hanging");
 
     /*
     |--------------------------------------------------------------------------
@@ -200,7 +200,7 @@
 
       let player = rows.append("g")
       .attr("transform", d => `translate(${i*(width/2)},0)`)
-      .call(hover, x)
+      .call(hover, x);
 
       /*
       |--------------------------------------------------------------------------
@@ -214,7 +214,7 @@
       .attr("width", x(game.duration))
       .attr("height", d => d.height)
       .attr("fill", d => color(d.category))
-      .attr("opacity", "0.1")
+      .attr("opacity", "0.1");
 
       /*
       |--------------------------------------------------------------------------
@@ -232,9 +232,9 @@
       .attr("y1", (d,i) => i*(line.height+line.gap))
       .attr("y2", (d,i) => i*(line.height+line.gap))
       .attr("stroke", (d,i,node) => {
-        let unit = getUnitFormNode(node)
+        let unit = getUnitFormNode(node);
         return color(unit.category)
-      })
+      });
 
       /*
       |--------------------------------------------------------------------------
@@ -260,21 +260,21 @@
     */
 
     // Modifiy margins
-    margin.top = 10
-    margin.right = 0
-    margin.bottom = 10
+    margin.top = 10;
+    margin.right = 0;
+    margin.bottom = 10;
 
     // Modify height
-    fullHeight = 250
-    height = fullHeight - margin.top - margin.bottom
+    fullHeight = 250;
+    height = fullHeight - margin.top - margin.bottom;
 
     // Select new SVG
-    svg = d3.select("#aggregation").attr("height", fullHeight)
+    svg = d3.select("#aggregation").attr("height", fullHeight);
 
     // Create base group
     g = svg
     .append("g")
-    .attr("transform", `translate(${margin.left} ${margin.top})`)
+    .attr("transform", `translate(${margin.left} ${margin.top})`);
 
     /*
     |--------------------------------------------------------------------------
@@ -292,7 +292,7 @@
 
       let player = g.append("g")
       .attr("transform", d => `translate(${i*(width/2)},0)`)
-      .call(hover, x)
+      .call(hover, x);
 
       /*
       |--------------------------------------------------------------------------
@@ -305,7 +305,7 @@
       .attr("y", 0)
       .attr("width", x(game.duration))
       .attr("height", height)
-      .attr("fill", "#f1f1f1")
+      .attr("fill", "#f1f1f1");
 
       /*
       |--------------------------------------------------------------------------
@@ -338,37 +338,37 @@
     .data(uniq(data.units.map(u => u.category)))
     .enter()
     .append("div")
-    .attr("class", "row")
+    .attr("class", "row");
 
     let tooltipTitle = tooltipRows.append("h3")
-    .attr("class", "title is-6")
+    .attr("class", "title is-6");
 
     tooltipTitle.append("span")
     .attr("class", "dot")
-    .attr("style", d => `background-color: ${color(d)}`)
+    .attr("style", d => `background-color: ${color(d)}`);
 
-    tooltipTitle.append("span").text(d => d.capitalize())
+    tooltipTitle.append("span").text(d => d.capitalize());
 
     let tooltipUnits = tooltipRows
     .selectAll(".unit")
-    .data(d => data.units.filter(u => u.category == d))
+    .data(d => data.units.filter(u => u.category === d))
     .enter()
     .append("div")
-    .attr("class", "unit")
+    .attr("class", "unit");
 
     tooltipUnits = tooltipUnits.append("div")
-    .attr("class", "level")
+    .attr("class", "level");
 
     tooltipUnits.append("div")
     .attr("class", "name")
-    .text(d => d.name)
+    .text(d => d.name);
 
     let counts = tooltipUnits.append("div")
-    .attr("class", "count")
+    .attr("class", "count");
 
-    counts.append("span").attr("id", d => `tooltip-${d.id}-0`).text(d => data.players[0].unit_counts[d.id][1000])
-    counts.append("span").text("-")
-    counts.append("span").attr("id", d => `tooltip-${d.id}-1`).text(d => data.players[1].unit_counts[d.id][1000])
+    counts.append("span").attr("id", d => `tooltip-${d.id}-0`).text(d => data.players[0].unit_counts[d.id][1000]);
+    counts.append("span").text("-");
+    counts.append("span").attr("id", d => `tooltip-${d.id}-1`).text(d => data.players[1].unit_counts[d.id][1000]);
 
     /**
      * React to mouse actions over a graph
@@ -378,17 +378,17 @@
      */
     function hover (g, x) {
 
-      g.style("position", "relative")
+      g.style("position", "relative");
       
       g.on("mousemove", moved)
         .on("mouseenter", entered)
-        .on("mouseleave", left)
+        .on("mouseleave", left);
 
       function moved () {
-        d3.event.preventDefault()
-        let eventX = d3.mouse(this)[0]
-        const xm = x.invert(eventX)
-        const i = Math.floor(xm)
+        d3.event.preventDefault();
+        let eventX = d3.mouse(this)[0];
+        const xm = x.invert(eventX);
+        const i = Math.floor(xm);
         interaction(i, d3.event)
       }
 
@@ -413,16 +413,16 @@
         // Show line
         d3.selectAll(".interaction-line")
         .attr("display", "inline")
-        .attr("transform", `translate(${x(time)},0)`)
+        .attr("transform", `translate(${x(time)},0)`);
 
         // Show tooltip
         let tooltipNode = d3.select("#tooltip")
-        .attr("class", "is-active")
+        .attr("class", "is-active");
 
-        let tooltipHeight = tooltipNode.node().clientHeight
+        let tooltipHeight = tooltipNode.node().clientHeight;
         
-        let xTranslation = event.screenX - tooltip.width - tooltip.spacing
-        let yTranslation = event.screenY - 100
+        let xTranslation = event.screenX - tooltip.width - tooltip.spacing;
+        let yTranslation = event.screenY - 100;
         
         if (window.innerWidth - event.screenX > tooltip.width + tooltip.spacing + 20) {
           xTranslation = event.screenX + tooltip.spacing 
@@ -432,18 +432,18 @@
           yTranslation = event.screenY - 100 - tooltipHeight
         }
 
-        tooltipNode.attr("style", `transform: translate(${xTranslation}px,${yTranslation}px)`)
+        tooltipNode.attr("style", `transform: translate(${xTranslation}px,${yTranslation}px)`);
 
         // Update data displayed in tooltip
         data.units.forEach(u => {
-          d3.select(`#tooltip-${u.id}-0`).text(d => data.players[0].unit_counts[d.id][time])
+          d3.select(`#tooltip-${u.id}-0`).text(d => data.players[0].unit_counts[d.id][time]);
           d3.select(`#tooltip-${u.id}-1`).text(d => data.players[1].unit_counts[d.id][time])
         })
 
       } else {
         // Hide line
         d3.selectAll(".interaction-line")
-        .attr("display", "none")
+        .attr("display", "none");
 
         // Hide tooltip
         d3.select("#tooltip")
@@ -475,23 +475,23 @@ function generateRandomData(duration) {
     { id: 'ipsum', name: 'Ipsum', category: 'flying' },
     { id: 'tempest', name: 'Tempest', category: 'flying' },
     { id: 'phoenix', name: 'Phoenix', category: 'flying' },
-  ]
+  ];
 
   data = {
     units: units,
     players: []
-  }
+  };
 
   for (let i = 0; i < 2; i++) {
 
     data.players[i] = {
       unit_lifetimes: {},
       unit_counts: {}
-    }
+    };
 
     units.forEach(u => {
-      data.players[i].unit_lifetimes[u.id] = []
-      data.players[i].unit_counts[u.id] = []
+      data.players[i].unit_lifetimes[u.id] = [];
+      data.players[i].unit_counts[u.id] = [];
 
       // Generate unit lifetimes 
       for (let j = 0; j < numberBetween(1, 200); j++) {
@@ -513,7 +513,7 @@ function generateRandomData(duration) {
 /** Capitalize */
 String.prototype.capitalize = function () {
   return this.charAt(0).toUpperCase() + this.slice(1)
-}
+};
 
 /**
  * Generate a number between a and b
@@ -532,8 +532,8 @@ function numberBetween(a, b) {
  * @param {*} b 
  */
 function generateRange(a, b) {
-  let a_ = numberBetween(a, b)
-  let b_ = numberBetween(a, b)
+  let a_ = numberBetween(a, b);
+  let b_ = numberBetween(a, b);
 
   if (a_ >= b_) {
     return [b_, a_]
@@ -542,12 +542,12 @@ function generateRange(a, b) {
 }
 
 function getUnitFormNode(node) {
-  let unitId = d3.select(node).node()[0].parentNode.parentNode.parentNode.getAttribute('data-unit-id')
-  return data.units.filter(u => u.id == unitId)[0]
+  let unitId = d3.select(node).node()[0].parentNode.parentNode.parentNode.getAttribute('data-unit-id');
+  return data.units.filter(u => u.id === unitId)[0]
 }
 
 function uniq(a) {
-  var seen = {};
+  let seen = {};
   return a.filter(function(item) {
       return seen.hasOwnProperty(item) ? false : (seen[item] = true);
   })
