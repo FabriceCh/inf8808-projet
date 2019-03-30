9689c826d980ba33ac11536940b5a5ccc31cf6f4
Adam Martin-Côté <adammartincote@gmail.com>
2019-03-30 16:52:58 -0600

    creates generate_unitcounts

--- data/dataGenerator.py
+++ data/dataGenerator.py
@@ -2,10 +2,22 @@
 
 import pysc2
 import json
+from copy import deepcopy
 
 REPLAY_FILE = 'replays/Harstem-vs-ShoWTimE-time1652.SC2Replay'
 OUTPUT_PATH = 'datafiles/'
-OUTPUT_FILE = 'unitcomposition_data.json'
+OUTPUT_FILE = 'unitcomposition_data2.json'
+
+
+def generate_unitcounts(**kwargs):
+    lifetime = kwargs.get('lifetime_dict')
+    unit_counts = {}
+    for unit in lifetime:
+        unit_name = unit['unit_type']
+        if unit_name not in unit_counts:
+            unit_counts[unit_name] = []
+
+    # print(unit_counts)
 
 
 def generate_unitcomposition_data(**kwargs):
@@ -13,12 +25,19 @@ def generate_unitcomposition_data(**kwargs):
     replay.categorize_unit_lifetime_events()
     categories = replay.categorize_unit_lifetime_events()
     processed_data = pysc2.match_events_to_units(categories)
-    post_post_processed = pysc2.prepare_data_for_visualisation(processed_data)
+    unitcomposition = pysc2.prepare_data_for_visualisation(processed_data)
 
-    json.dumps(post_post_processed)
+    for key, value in unitcomposition.items():
+        unit_lifetimes = value['unit_lifetimes']
+        # print(value['unit_counts'])
+        unit_counts = generate_unitcounts(lifetime_dict=unit_lifetimes)
+        # value['unit_counts'] = [1,2,3]
+
+    print(unitcomposition["p1"]['unit_counts'])
+
+    # json.dumps(unitcomposition)
     with open(OUTPUT_PATH + kwargs.get('output'), 'w+') as f:
-        f.write(json.dumps(post_post_processed, indent=2))
-    pass
+        f.write(json.dumps(unitcomposition, indent=2))
 
 
 if __name__ == "__main__":
