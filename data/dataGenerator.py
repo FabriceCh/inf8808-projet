89a374acf65325e250947d49acad3d4654f9b5ed
Adam Martin-Côté <adammartincote@gmail.com>
2019-03-30 14:11:51 -0600

    factor out filenames in dataGenerator

--- data/dataGenerator.py
+++ data/dataGenerator.py
@@ -3,22 +3,25 @@
 import pysc2
 import json
 
+REPLAY_FILE = 'replays/Harstem-vs-ShoWTimE-time1652.SC2Replay'
+OUTPUT_PATH = 'datafiles/'
+OUTPUT_FILE = 'unitcomposition_data.json'
 
-replay_filename = 'replays/Harstem-vs-ShoWTimE-time1652.SC2Replay'
-replay = pysc2.SC2ReplayWrapper(replay_filename)
 
-
-def generate_unitcomposition_data():
+def generate_unitcomposition_data(**kwargs):
+    replay = pysc2.SC2ReplayWrapper(kwargs.get('replay'))
     replay.categorize_unit_lifetime_events()
     categories = replay.categorize_unit_lifetime_events()
     processed_data = pysc2.match_events_to_units(categories)
     post_post_processed = pysc2.prepare_data_for_visualisation(processed_data)
 
     json.dumps(post_post_processed)
-    with open('datafiles/unitcomposition/mydata.json', 'w+') as f:
+    with open(OUTPUT_PATH + kwargs.get('output'), 'w+') as f:
         f.write(json.dumps(post_post_processed, indent=2))
     pass
 
 
 if __name__ == "__main__":
-    generate_unitcomposition_data()
+    generate_unitcomposition_data(
+        replay=REPLAY_FILE,
+        output=OUTPUT_FILE)
