2ef508e450ce2ffd9d4716ec192a286787e638be
François Lévesque <francois@witify.io>
2019-03-30 14:37:09 -0400

    Add units icons and fix interaction hiccups

--- frontend/js/units.js
+++ frontend/js/units.js
@@ -13,7 +13,7 @@
 
     let margin = {
       top: 50,
-      left: 120,
+      left: 150,
       right: 0,
       bottom: 10
     };
@@ -29,8 +29,8 @@
     let row = {
       // Margin between rows
       margin: {
-        top: 10,
-        bottom: 10
+        top: 5,
+        bottom: 5
       }
     };
     
@@ -70,18 +70,19 @@
     // For each unit, the height and vertial offset is calculated 
     // to correctly position the rows on the graph.
     let offset = 0;
+
     data.units = data.units.map(u => {
-      u.offset = offset + row.margin.top;
-      u.height = d3.max(data.players, p =>p.unit_lifetimes[u.id].length) * (line.height+line.gap);
-      offset += u.height + row.margin.bottom;
-      return u
+      u.offset = offset;
+      u.height = d3.max(data.players, p =>p.unit_lifetimes[u.id].length) * (line.height+line.gap) + row.margin.top + row.margin.bottom;
+      offset += u.height;
+      return u;
     });
 
     // Sort unit_lifetimes 
     data.players.forEach(p => {
       Object.keys(p.unit_lifetimes).forEach(key => {
         p.unit_lifetimes[key] = p.unit_lifetimes[key].sort((a,b) => {
-          return (a[0] > b[0]) ? 1 : -1
+          return (a[0] > b[0]) ? 1 : -1;
         })
       })
     });
@@ -93,7 +94,7 @@
     */
 
     let fullHeight = offset;
-    let height = fullHeight - margin.top - margin.bottom;
+    let height = fullHeight;
 
     /*
     |--------------------------------------------------------------------------
@@ -179,10 +180,22 @@
     */
 
     rows.append("text")
-    .attr("x", -margin.left)
-    .attr("y", 0)
+    .attr("x", -margin.left + 40)
+    .attr("y", 5)
     .text(d => d.name)
     .attr("alignment-baseline", "hanging");
+
+    /*
+    |--------------------------------------------------------------------------
+    | Row : Left Icon
+    |--------------------------------------------------------------------------
+    */
+
+    rows.append("svg:image")
+    .attr("x", -margin.left)
+    .attr("y", 0)
+    .attr("width", 30)
+    .attr("xlink:href", d => `/img/icons/${d.id}.png`);
 
     /*
     |--------------------------------------------------------------------------
@@ -211,8 +224,15 @@
       player.append("rect")
       .attr("x", 0)
       .attr("y", 0)
-      .attr("width", x(game.duration))
+      .attr("width", x(game.duration) + column.gap)
       .attr("height", d => d.height)
+      .attr("fill", "#fff");
+
+      player.append("rect")
+      .attr("x", 0)
+      .attr("y", 0)
+      .attr("width", x(game.duration))
+      .attr("height", d => d.height - row.margin.top - row.margin.bottom)
       .attr("fill", d => color(d.category))
       .attr("opacity", "0.1");
 
@@ -247,7 +267,7 @@
       .attr("x1", 0)
       .attr("x2", 0)
       .attr("y1", 0)
-      .attr("y2", d => d.height)
+      .attr("y2", d => d.height - row.margin.top - row.margin.bottom)
       .attr("stroke", "#000")
       .attr("display", "none")
 
@@ -260,13 +280,21 @@
     */
 
     // Modifiy margins
-    margin.top = 10;
+    margin.top = 0;
     margin.right = 0;
-    margin.bottom = 10;
+    margin.bottom = 0;
+
+    let padding = {
+      top: 10,
+      bottom: 10,
+      left: 0,
+      right: 0
+    };
 
     // Modify height
     fullHeight = 250;
     height = fullHeight - margin.top - margin.bottom;
+    innerHeight = height - padding.top - padding.bottom;
 
     // Select new SVG
     svg = d3.select("#aggregation").attr("height", fullHeight);
@@ -286,13 +314,35 @@
 
       /*
       |--------------------------------------------------------------------------
-      | Row : Player (group for each player)
+      | Row : Player : Group for each player
       |--------------------------------------------------------------------------
       */
 
-      let player = g.append("g")
+      let content = g.append("g")
       .attr("transform", d => `translate(${i*(width/2)},0)`)
       .call(hover, x);
+
+      /*
+      |--------------------------------------------------------------------------
+      | Row : Player : White Rectangle for interaction
+      |--------------------------------------------------------------------------
+      */
+
+      content.append("rect")
+      .attr("x", 0)
+      .attr("y", 0)
+      .attr("width", x(game.duration) + column.gap)
+      .attr("height", fullHeight)
+      .attr("fill", "#fff");
+
+      /*
+      |--------------------------------------------------------------------------
+      | Row : Player : Group for drawing
+      |--------------------------------------------------------------------------
+      */
+
+      let player = content.append("g")
+      .attr("transform", d => `translate(0,${padding.top})`);
 
       /*
       |--------------------------------------------------------------------------
@@ -304,7 +354,7 @@
       .attr("x", 0)
       .attr("y", 0)
       .attr("width", x(game.duration))
-      .attr("height", height)
+      .attr("height", innerHeight)
       .attr("fill", "#f1f1f1");
 
       /*
@@ -317,7 +367,7 @@
       const MAX_UNIT_N = 120;
       let y = d3
           .scaleLinear()
-          .range([height, 0])
+          .range([innerHeight, 0])
           .domain([0, MAX_UNIT_N]);
 
       // set the ranges
@@ -374,7 +424,7 @@
       .attr("x1", 0)
       .attr("x2", 0)
       .attr("y1", 0)
-      .attr("y2", height)
+      .attr("y2", innerHeight)
       .attr("stroke", "#000")
       .attr("display", "none")
   }
@@ -444,7 +494,7 @@
         let eventX = d3.mouse(this)[0];
         const xm = x.invert(eventX);
         const i = Math.floor(xm);
-        interaction(i, d3.event)
+        interaction(i, d3.event);
       }
 
       function entered () {
@@ -502,8 +552,13 @@
 
         // Hide tooltip
         d3.select("#tooltip")
-        .attr("class", "")
+        .attr("class", "");
+      }
 
+      if (time > game.duration) {
+        // Hide tooltip
+        d3.select("#tooltip")
+        .attr("class", "");
       }
     }
     
@@ -519,18 +574,29 @@
 function generateRandomData(duration) {
 
   let units = [
-    { id: 'probe', name: 'Probe', category: 'resource' },
-    { id: 'zealot', name: 'Zealot', category: 'offensive' },
+    { id: 'adept', name: 'Adept', category: 'resource' },
+    { id: 'archon', name: 'Archon', category: 'resource' },
+    { id: 'carrier', name: 'Carrier', category: 'resource' },
+    { id: 'colossus', name: 'Colossus', category: 'offensive' },
+    { id: 'dark-templar', name: 'Dark Templar', category: 'offensive' },
+    { id: 'disruptor', name: 'Disruptor', category: 'offensive' },
     { id: 'high-templar', name: 'High Templar', category: 'offensive' },
     { id: 'immortal', name: 'Immortal', category: 'offensive' },
-    { id: 'chuck-norris', name: 'Chuck Norris', category: 'offensive' },
+    { id: 'mothership', name: 'Mothership', category: 'scout' },
     { id: 'observer', name: 'Observer', category: 'scout' },
-    { id: 'batman', name: 'Batman', category: 'scout' },
-    { id: 'lorem', name: 'Lorem', category: 'flying' },
-    { id: 'ipsum', name: 'Ipsum', category: 'flying' },
-    { id: 'tempest', name: 'Tempest', category: 'flying' },
     { id: 'phoenix', name: 'Phoenix', category: 'flying' },
+    { id: 'probe', name: 'Probe', category: 'resource' },
+    { id: 'sentry', name: 'Sentry', category: 'flying' },
+    { id: 'stalker', name: 'Stalker', category: 'flying' },
+    { id: 'tempest', name: 'Tempest', category: 'flying' },
+    { id: 'void-ray', name: 'Void Ray', category: 'offensive' },
+    { id: 'warp-prism', name: 'Warp Prism', category: 'offensive' },
+    { id: 'zealot', name: 'Zealot', category: 'offensive' },
   ];
+
+  units = units.sort((a,b) => {
+    return (a.category > b.category) ? 1 : -1
+  })
 
   data = {
     units: units,
