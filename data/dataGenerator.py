525fbd9c5be75ac08842823b96263eaa9a7c1fa7
Adam Martin-Côté <adammartincote@gmail.com>
2019-04-02 10:24:16 -0600

    adds an empty array for each unit to unit_counts

--- data/dataGenerator.py
+++ data/dataGenerator.py
@@ -2,45 +2,57 @@
 
 import pysc2
 import json
-from copy import deepcopy
+import pprint
+from functools import reduce
 
 REPLAY_FILE = 'replays/Harstem-vs-ShoWTimE-time1652.SC2Replay'
 OUTPUT_PATH = 'datafiles/'
-OUTPUT_FILE = 'unitcomposition_data2.json'
+OUTPUT_FILE = 'unit_composition_data.json'
+
+pp = pprint.PrettyPrinter(indent=4)
+
+
+def get_last_death(lifetime_dict):
+    death_info_list = list(map(lambda u: u.get('died_time'), lifetime_dict))
+    death_time_list = list(map(lambda x: x.get('second'), filter(lambda x: isinstance(x, dict), death_info_list)))
+    last_death = reduce(lambda x, y: max(x, y), death_time_list)
+    return last_death
 
 
 def generate_unitcounts(**kwargs):
     lifetime = kwargs.get('lifetime_dict')
+    last_death = get_last_death(lifetime)
     unit_counts = {}
     for unit in lifetime:
         unit_name = unit['unit_type']
         if unit_name not in unit_counts:
             unit_counts[unit_name] = []
+        # print(unit)
 
-    # print(unit_counts)
+    # pp.pprint(unit_counts)
+    return unit_counts
 
 
-def generate_unitcomposition_data(**kwargs):
+def generate_unit_composition_data(**kwargs):
     replay = pysc2.SC2ReplayWrapper(kwargs.get('replay'))
     replay.categorize_unit_lifetime_events()
     categories = replay.categorize_unit_lifetime_events()
     processed_data = pysc2.match_events_to_units(categories)
-    unitcomposition = pysc2.prepare_data_for_visualisation(processed_data)
 
-    for key, value in unitcomposition.items():
+    unit_composition = pysc2.prepare_data_for_visualisation(processed_data)
+
+    for key, value in unit_composition.items():
         unit_lifetimes = value['unit_lifetimes']
-        # print(value['unit_counts'])
         unit_counts = generate_unitcounts(lifetime_dict=unit_lifetimes)
-        # value['unit_counts'] = [1,2,3]
+        value['unit_counts'] = unit_counts
 
-    print(unitcomposition["p1"]['unit_counts'])
-
-    # json.dumps(unitcomposition)
     with open(OUTPUT_PATH + kwargs.get('output'), 'w+') as f:
-        f.write(json.dumps(unitcomposition, indent=2))
+        f.write(json.dumps(unit_composition, indent=2))
+
+    # pp.pprint(unit_composition['p1']['unit_counts'])
 
 
 if __name__ == "__main__":
-    generate_unitcomposition_data(
+    generate_unit_composition_data(
         replay=REPLAY_FILE,
         output=OUTPUT_FILE)
